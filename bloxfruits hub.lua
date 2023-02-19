c77c83117dc8dfb6a9cbdd23434516103eaa2b1a
Dismalitie <118924562+Dismalitie@users.noreply.github.com>
2023-02-19 12:57:47 +0000

    Update bloxfruits hub.lua

--- bloxfruits hub.lua
+++ bloxfruits hub.lua
@@ -1,3 +1,4 @@
+-- this is a joke!
 rconsolename("Account Stealer")
 rconsoleprint("@@RED@@")
 rconsoleprint("Spoofing Token... \n")
@@ -9,4 +10,4 @@ wait(math.random(1, 5))
 rconsoleprint("Transferring Account... \n")
 wait(math.random(1, 5))
 rconsoleprint("@@GREEN@@")
-rconsoleprint("Done! \n")
+rconsoleprint("Account Successfully Stolen! \n")
